d352b51a7fd407fef483dbab99687f70d18513ab
Rodrigo <rodrigo.martinez@escrol.es>
2022-11-10 13:24:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# shoutrrr alertmanager bridge
+
+A basic alertmanager bridge to shoutrrr.
+
+Created by modifying code from https://github.com/aTable/ntfy_alertmanager_bridge
+
+It sends a shoutrrr send command PER ALERT, so if in one request you have 4 alerts, it will send 4 times
+
+This bridge receives data in [this format](https://prometheus.io/docs/alerting/latest/notifications/#alert) in the [alertmanager webhook config](alertmanager/alertmanager/config.yml):
+
+```sh
+...
+receivers:
+  - name: "shoutrrr-servers"
+    webhook_configs:
+      - url: http://shoutrrr_alertmanager_bridge:30000/shoutrrr_alert/<serviceName>?<shoutrrrParams>
+        send_resolved: true
+        max_alerts: 0
+...
+```
+`shoutrrrParams` will be added to the shoutrrr service URL automatically
+
+You also need to create a file with your services, and other whit Handlebars templates and pass them to Docker in these 2 vars:
+```sh
+environment:
+  - SERVICES_CONFIG=/etc/s_a_b/services.json
+  - TEMPLATES_PATH=/etc/s_a_b/templates/
+```
+or better, mount a volume in `/etc/s_a_b/` with services.json and template folder
+
+Services.json file expect a JSON Array with your configuration for shoutrrr services.
+
+```sh
+[
+    {
+        "name": "Give_me_a_cool_name_please",
+        "url": "shoutrrr_url_from_service",
+        "template": "cooltemplatename.hbs",
+        "replaceInURL": [
+            {
+                "shoutrrrProp": "Subject",
+                "by": "fingerprint",
+                "byAlert": true
+            }
+        ]
+    }
+    ...
+]
+```
+You need to give a name, which will be used in your receiver's URL from alertmanager.
+
+Also, you have to put a shoutrrr service URL, the name of the template, and if you want, you can replace dynamically some values in the shoutrrr service URL. byAlert means look in every alert, and not in the JSON posted
+
+The [docker-compose.yml](docker-compose.yml) contains a prometheus+alertmanager+shoutrrr_alertmanager_bridge if you want to run the stack as is. Or if you want to pull just the shoutrrr_alertmanager_bridge simply add to your docker-compose.yml:
+
+```sh
+...
+
+  shoutrrr_alertmanager_bridge:
+    image: ghcr.io/rodriciru/shoutrrr_alertmanager_bridge:main
+    container_name: shoutrrr_alertmanager_bridge
+    ports:
+      - 30000:30000
+    volumes:
+      - ./s_a_b/:/etc/s_a_b/
+    #environment:
+      #- SERVICES_CONFIG=/etc/s_a_b/services.json
+      #- TEMPLATES_PATH=/etc/s_a_b/templates/
+
+...
+```
+
+Do note this is just a prototype, it works FOR ME, but maybe for you, it works as well.
+
+I accept pull request, or any improvements.
+
+## Development
+
+```sh
+docker-compose up
+```
---
+++ src/server.js
@@ -0,0 +1,108 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const Koa = require("koa");
+const Router = require("@koa/router");
+const bodyParser = require("koa-bodyparser");
+const { execSync } = require("child_process");
+const Handlebars = require("handlebars");
+
+const config = {
+  port: process.env.PORT || 30000,
+  servicesConfigPath: process.env.SERVICES_CONFIG || "/etc/s_a_b/services.json",
+  templatesPath: process.env.TEMPLATES_PATH || "/etc/s_a_b/templates/",
+  tempFolderPath: undefined,
+};
+
+fs.mkdtemp(path.join(os.tmpdir(), "shoutrrr_alertmanager_bridge"), (err, folder) => {
+  if (err) throw err;
+  console.log("created temp folder: " + folder);
+  config.tempFolderPath = folder;
+});
+console.log("Reading services from: " + config.servicesConfigPath);
+let servicesJSON = JSON.parse(fs.readFileSync(config.servicesConfigPath));
+
+const app = new Koa();
+app.use(bodyParser());
+const router = new Router();
+
+function cleanShellOutput(source) {
+  return source.trimEnd("\r\n").replaceAll('"', "");
+}
+
+function logIncomingRequest(ctx) {
+  const incoming = {
+    url: ctx.URL,
+    headers: ctx.headers,
+    body: ctx.request.body,
+  };
+  const serialized = JSON.stringify(incoming, null, 2);
+  const now = new Date();
+  const logFileName = path.join(
+    config.tempFolderPath,
+    `req-${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}T${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}.log`
+  );
+  fs.writeFileSync(logFileName, serialized);
+}
+
+router
+  .get("/", (ctx, next) => {
+    ctx.body = "hello world";
+  })
+  .post("/shoutrrr_alert/:service", (ctx, next) => {
+    logIncomingRequest(ctx);
+    //Get service data from JSON
+    let service = servicesJSON.filter(function (item) { return item.name === ctx.params.service; })[0];
+
+    if (service !== undefined) { // Cool! We have a configured service
+      let templateSource = fs.readFileSync(config.templatesPath + service.template).toString();
+      let template = Handlebars.compile(templateSource);
+      for (const x of ctx.request.body.alerts) {
+        let url = new URL(service.url);
+        let search_params = url.searchParams;
+
+        // Remove CRLF from query
+        Object.keys(ctx.query).reduce((acc, key) => {
+          acc[key] = cleanShellOutput(ctx.query[key]);
+          return acc;
+        }, {});
+
+        // Set URL query values per receivers URL
+        Object.entries(ctx.query).forEach(([key, value]) => {
+          search_params.set(key, value);
+        });
+
+        // Dinamically replace values in Shoutrrr service URL
+        service.replaceInURL.forEach((rip) => {
+          if (rip.byAlert) {
+            search_params.set(rip.shoutrrrProp, x[rip.by]);
+          } else {
+            search_params.set(rip.shoutrrrProp, ctx.request.body[rip.by]);
+          }
+        });
+        
+        url.search = search_params.toString();
+        url = (decodeURIComponent(url));
+        var html = template(x).replaceAll('"', "\\\"");
+
+        console.log(`Sending to ${url}\n${html}`);
+        try {
+          execSync(`shoutrrr send -m "${html}" -u "${url}"`);
+          ctx.body = "tnx alertmanager";
+        } catch (error) {
+          console.log(`Error\n ${error}`);
+          ctx.body = `Error sending shoutrrr command:\n${error}`;
+          ctx.status = 500
+          break;
+        }
+      };
+
+    } else {
+      console.log(`No service "${ctx.params.service}" found in ${config.servicesConfigPath}`);
+      ctx.body = `No service "${ctx.params.service}" found in ${config.servicesConfigPath}`;
+      ctx.status = 404
+    }
+  });
+
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(config.port);
